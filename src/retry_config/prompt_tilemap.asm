2733c3aa89b6e8d6c1bbcbb42956044eb301364d
Kevin M. <kevin_495@hotmail.it>
2026-02-05 00:22:21 +0100

    Written prompt tilemap doc, started working on tilemap generator tool

--- src/retry_config/prompt_tilemap.asm
+++ src/retry_config/prompt_tilemap.asm
@@ -2,14 +2,13 @@
 ; If you're opening this just to change which areas of VRAM (SP1/SP2/SP3/SP4)
 ; are used by the Retry prompt tiles, then look at the "Basic settings".
 ; To also change the prompt text, look at "Advanced settings" (note: more complicated).
+; For more detailed explanations and an example, look at the "prompt_tilemap.html" documentation page.
 
 ;============================== Basic settings =================================
 
 ; Sprite tile numbers for the tiles used by the prompt ($00-$FF = SP1/SP2, $100-$1FF = SP3/SP4).
 ; !prompt_tile_cursor is the cursor and space tile, !prompt_tiles_line1 are the unique letters for the
 ; first line, !prompt_tiles_line2 are the unique letters for the second line.
-; The default values should be fine, but you may need to change them if using some other patches
-; that upload tiles dynamically (like 32x32 Mario or lx5's Custom Powerups).
 ; Note: When the prompt black box is enabled, !prompt_tile_cursor uses three adjacent 8x8 tiles.
 ;       When the prompt black box is disabled, !prompt_tile_cursor uses one 8x8 tile.
 ; Note: When the exit option is disabled, the line2 list is unused.
@@ -25,9 +24,6 @@
 
 ; These settings specify the index of the prompt tiles in the GFX bin file.
 ; By index it means at which 8x8 tile (starting from the top left) the tiles are found (starting from 0).
-; You usually shouldn't edit the cursor, but you can edit the line1 and line2 to
-; change which tiles from the GFX file are used for the first and second line of the prompt.
-; Of course, you also need to edit the GFX files if you want to add/remove letters.
 ; If you edit this, make sure that !prompt_gfx_index_line1 has the same amount of values as
 ; !prompt_tiles_line1, and that !prompt_gfx_index_line2 has the same amount of values as !prompt_tiles_line2.
 ; Note: When the exit option is disabled, the line2 list is unused.
@@ -36,8 +32,8 @@
     !prompt_gfx_index_line2  = 7,8     ; XI
 
 ; These settings specify which tiles are drawn on the two lines of the Retry prompt, and in which order.
-; To find these out, take the two list of tiles !prompt_tiles_line1 and !prompt_tiles_line2 together and imagine them as a single list.
-; This list corresponds to the available letters you can use (by default, R-E-T-Y-X-I).
+; To find these out, take the two list of tiles !prompt_gfx_index_line1 and !prompt_gfx_index_line2 together
+; and imagine them as a single list (with the default, it's the list R-E-T-Y-X-I).
 ; Then, for both lines you specifiy the index of the letters in this list, starting from 0.
 ; For example, with the default tiles, 0 = R, 1 = E, 2 = T, 3 = Y, so "RETRY" becomes "0,1,2,0,3".
 ; Note that if the black box is enabled, the letters amount is constrained by the box size.
