aa34334e5d9d8fd7c200bcacef49681bda4cc4cf
Kevin M. <kevin_495@hotmail.it>
2026-03-28 00:17:08 +0100

    Version 2.0.1

--- src/retry_config/code/include/misc.asm
+++ src/retry_config/code/include/misc.asm
@@ -2,7 +2,7 @@
 ; You usually shouldn't edit this file.
 
 ; Retry version number to write in ROM.
-!version = "2.0.0"
+!version = "2.0.1"
 
 ; Print version in the terminal
 print "    Retry System version: !version"
